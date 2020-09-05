c6e20f25c7c3387c79260fc21d284b4e9742f2f7
luomuxie <835657341@qq.com>
2020-09-05 22:30:22 +0800

    add screeps

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { create } from "lodash";
+module.exports.loop = function(): void {
+    findSources();
+}
+
+
+function findSources():void{
+    const creep = Game.creeps['Harvester1'];
+    if(creep.store.getFreeCapacity()>0){
+        const sources = creep.room.find(FIND_SOURCES);    
+        if(creep.harvest[sources[0] as any] == ERR_NOT_IN_RANGE){
+            creep.moveTo(sources[0]);
+        }
+    }else{
+        if(creep.transfer(Game.spawns['Spawn1'],RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+            creep.moveTo(Game.spawns['Spawn1']);
+        }
+    }
+    
+}
